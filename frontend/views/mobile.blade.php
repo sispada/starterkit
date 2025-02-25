f3d68d59c7c38e5686ed751d9dc2ed84effac28f
monoland <mono.socialmedia@gmail.com>
2025-02-25 08:41:49 +0700

    update

--- frontend/views/mobile.blade.php
+++ frontend/views/mobile.blade.php
@@ -49,7 +49,7 @@
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
         rel="stylesheet" />
 
-    <title>SiLEGIT</title>
+    <title>SiASEP</title>
 
     @vite('resources/src/mobile.js')
 </head>
